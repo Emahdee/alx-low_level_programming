8fc64aea0cc2d97700c9a220670eda183fb9573b
root <root@Daniel-PC.localdomain>
2022-09-08 17:04:57 +0100

    Committed from script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 /* Using the puts() function */
 
-int main()
+int main(void)
 {
 	/* string initialisation */
 	char Test[] = "Programming is like building a multilingual puzzle";
 
 	puts(Test); /* writing the string to stdout */
 
-	return(0);
+	return (0);
 }
