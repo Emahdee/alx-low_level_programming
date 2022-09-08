d3061f8098bc7c5a2af098e61f60927a48d82717
root <root@Daniel-PC.localdomain>
2022-09-08 17:20:47 +0100

    Committed from script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
