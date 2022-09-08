044aed338ea4310f44759a82a95dde629f34461a
Daniel Emah <emahdaniel123@gmail.com>
2022-09-08 08:42:04 -0700

    Committed from script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Using the puts() function */
+
+int main()
+{
+	/* string initialisation */
+	char Test[] = "Programming is like building a multilingual puzzle";
+
+	puts(Test); /* writing the string to stdout */
+
+	return(0);
+}
