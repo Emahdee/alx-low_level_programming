f0e5045f62d317cdc32b5d1c13c360366410323a
root <root@Daniel-PC.localdomain>
2022-09-08 17:22:44 +0100

    Committed from script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
