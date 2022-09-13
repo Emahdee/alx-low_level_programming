561a447df99882b36ab99662d10ce9894b63e371
root <root@Daniel-PC.localdomain>
2022-09-13 15:23:15 +0100

    Committed from script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char str[8] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
