1a637074b66f2ba6da653267be6c07bc66e3405a
root <root@Daniel-PC.localdomain>
2022-09-13 20:40:50 +0100

    Committed from script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - add two integers
+* @a: integer to be checked
+* @b: integer to be checked
+* Return: the result of the addition
+*/
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
