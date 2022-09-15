69731c95c3c8bc3549904956ec7734f3ff2de12d
root <root@Daniel-PC.localdomain>
2022-09-15 12:34:07 +0100

    Committed from script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* mul - multiply two integers
+* @a: integer to be checked
+* @b: integer to be checked
+*
+* Return: the answer for the multipe
+*/
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
