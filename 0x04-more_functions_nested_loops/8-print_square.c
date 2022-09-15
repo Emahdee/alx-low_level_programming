11f20686f20e34ecc797cfb25a4beb2020635004
root <root@Daniel-PC.localdomain>
2022-09-15 14:48:36 +0100

    Committed from script

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_square -  draws a straight line in the terminal.
+* @size: number to be checked
+*
+* Return:
+*/
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+
+			if (a == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
