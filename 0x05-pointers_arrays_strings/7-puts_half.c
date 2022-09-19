ee66d629c46a7e9712f449dc64b7cd32b3a22a1b
root <root@Daniel-PC.localdomain>
2022-09-19 18:34:00 +0100

    Committed from script

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,9 +15,10 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		strlen++;
 
-	j = strlen / 2;
-
 	if ((strlen % 2) == 0)
+		j = strlen / 2;
+
+	else 
 		j = ((strlen + 1) / 2);
 
 	for (i = j; str[i] != '\0'; i++)
