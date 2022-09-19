0e0e4bd3ddf5ed22175a84385c1f337eed6cc929
root <root@Daniel-PC.localdomain>
2022-09-19 14:47:55 +0100

    Committed from script

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - function that returns the length of a string.
+* @s: The character to be checked.
+*
+* Return:
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+
+	return (i);
+}
