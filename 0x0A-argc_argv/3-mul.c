18743d4d0de8ecbaff3a0fda0af3dcbbe9c9f113
root <root@Daniel-PC.localdomain>
2022-09-30 17:39:43 +0100

    Committed from script

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-    int mul, num1, num2;
+	int mul, num1, num2;
 
-    if (argc != 3)
-    {
-        printf("%s\n", "Error");
-        return (1);
-    }
-        
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
 
-    mul = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-    printf("%d\n", mul);
-            
-    return(0);
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+
+	return(0);
 }
