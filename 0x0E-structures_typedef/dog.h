52e71c49324e17ac4417397b02407b062eeee018
root <root@Daniel-PC.localdomain>
2022-10-10 17:01:09 +0100

    Committed from script

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog's details
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description - The dog's details
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+#endif /* DOG_H */
