a5351765e88d9991682e032eed836d1d30a97daf
root <root@Daniel-PC.localdomain>
2022-10-10 17:16:03 +0100

    Committed from script

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,18 @@
  * Description - The dog's details
 */
 
+/**
+ * dog_t - Typedef for struct dog
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
